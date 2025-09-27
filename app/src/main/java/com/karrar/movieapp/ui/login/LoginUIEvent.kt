56f917ad2a34196e21a5a3d613ab2de3dc4aefa5
Radwa85 <radwa.mohamed5033@gmail.com>
2025-09-27 03:02:16 +0300

    Update login UI events and navigation

--- app/src/main/java/com/karrar/movieapp/ui/login/LoginUIEvent.kt
+++ app/src/main/java/com/karrar/movieapp/ui/login/LoginUIEvent.kt
@@ -3,4 +3,6 @@ package com.karrar.movieapp.ui.login
 sealed interface LoginUIEvent {
     data class LoginEvent(val login: Int) : LoginUIEvent
     object SignUpEvent : LoginUIEvent
+    object ForgetPasswordEvent : LoginUIEvent
+    object JoinAsGuestEvent : LoginUIEvent
 }
